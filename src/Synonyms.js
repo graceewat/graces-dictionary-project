900930bd168e919ef87b13feeaaeb805507c9064
graceewat <160866251+graceewat@users.noreply.github.com>
2024-11-28 11:08:39 +0000

    updated sysnonyms.js to list out elements

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -2,8 +2,15 @@ import React from "react";
 import "./Synonyms.css";
 
 export default function Synonyms(props) {
+  console.log(props.synonyms);
   if (props.synonyms) {
-    return <div className="synonyms ">{props.synonyms}</div>;
+    return (
+      <ul className="synonyms">
+        {props.synonyms.map(function (synonym, index) {
+          return <li key={index}>{synonym}</li>;
+        })}
+      </ul>
+    );
   } else {
     return null;
   }
