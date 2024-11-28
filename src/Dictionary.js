a86eb9b48bca4f9ce69d52e44a86178dc9708da4
graceewat <160866251+graceewat@users.noreply.github.com>
2024-11-28 11:28:17 +0000

    Crated new component for photos using new API and added to results of search with formatting

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,10 +2,12 @@ import React, { useState } from "react";
 import "./Dictionary.css";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Dictionary() {
   let [keyWord, setKeyWord] = useState("");
   let [results, setResults] = useState(null);
+  let [photos, setPhotos] = useState(null);
   function handleResponse(response) {
     setResults(response.data);
   }
@@ -16,9 +18,17 @@ export default function Dictionary() {
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyWord}
       &key=${apiKey}`;
     axios.get(apiUrl).then(handleResponse);
+
+    let apiPhotoKey = "f03c7t04000f0dbod331aeefa47ad1f6";
+    let apiPhotoUrl = `https://api.shecodes.io/images/v1/search?query=${keyWord}&key=${apiPhotoKey}`;
+    let headers = { Authorization: `${apiPhotoKey}` };
+    axios.get(apiPhotoUrl, { headers: headers }).then(handlePhotosResponse);
   }
   function handleKeyWordChange(event) {
     setKeyWord(event.target.value);
+  }
+  function handlePhotosResponse(response) {
+    setPhotos(response.data.photos);
   }
   return (
     <div className="dictionary">
@@ -32,6 +42,7 @@ export default function Dictionary() {
         <input type="submit" />
       </form>
       <Results results={results} />
+      <Photos photos={photos} />
     </div>
   );
 }
